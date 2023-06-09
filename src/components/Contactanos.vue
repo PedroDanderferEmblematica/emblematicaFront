c570ea907e42b4e1fb7a35e4ad088d414aade316
Pedro <danderferpedro@gmail.com>
2023-06-09 12:33:43 -0300

    Version 1.0.1

--- src/components/Contactanos.vue
+++ src/components/Contactanos.vue
@@ -31,40 +31,54 @@ import axios from 'axios';
     <section id="Contactanos">
         <h2>Contactate con nosotros</h2>
         <div>
-            <a href="#"><div><div><span></span><span>Escribinos un whatsapp</span></div></div></a>
-            <ul>
-                <li><a href="#"><div><div><span></span><span>Instagram</span></div></div></a></li>
-                <li><a href="#"><div><div><span></span><span>LinkedIn</span></div></div></a></li>
-            </ul>
-            <ul>
-                <li><span>Número de teléfono: </span><span>+54 9 11 6666-6666</span></li>
-                <li><span>Correo electronico: </span><span>consultas@emblematica.ar</span></li>
-            </ul>
+            <div>
+                <a href="#"><div><div><span></span><span>Escribinos un whatsapp</span></div></div></a>
+                <ul>
+                    <li><a href="#"><div><div><span></span><span>Instagram</span></div></div></a></li>
+                    <li><a href="#"><div><div><span></span><span>LinkedIn</span></div></div></a></li>
+                </ul>
+                <ul>
+                    <li><span>Número de teléfono: </span><span>+54 9 11 6666-6666</span></li>
+                    <li><span>Correo electronico: </span><span>consultas@emblematica.ar</span></li>
+                </ul>
+            </div>
+            <div>
+                <h3>Envianos un mensaje</h3>
+                <form action="#" method="POST" @submit.prevent="formSubmit">
+                    <div>
+                        <input type="text" name="nombre" placeholder="Nombre">
+                    </div>
+                    <div>
+                        <input type="text" name="email" placeholder="Correo electronico">
+                    </div>
+                    <div>
+                        <textarea name="comentario" id="" cols="30" rows="10" placeholder="Dejanos tu comentario"></textarea>
+                    </div>
+                    <input type="submit" value="Enviar">
+                </form>
+            </div>
         </div>
-        <form action="#" method="POST" @submit.prevent="formSubmit">
-            <input type="text" name="nombre">
-            <input type="text" name="email">
-            <textarea name="comentario" id="" cols="30" rows="10"></textarea>
-            <input type="submit" value="Enviar">
-        </form>
     </section>
 </template>
 <style>
     #Contactanos{
-        padding-top: 20px;
-    }
-
-    #Contactanos h2{
-        margin-left: 10px;
+        padding: 20px 10px 10px 10px;
     }
 
     #Contactanos >div{
-        padding: 10px;
+        margin-top: 10px;
+        display: grid;
+        grid-template-columns: 1fr;
+        grid-template-rows: 1fr auto;
+        grid-gap: 5px;
+    }
+
+    #Contactanos >div >div:first-of-type{
         display: grid;
         grid-template-columns: 1fr;
         grid-template-rows: 1fr 1fr;
     }
-    #Contactanos >div >a{
+    #Contactanos >div >div:first-of-type >a{
         display: block;
         width:100%;
         background-color: #25d366;
@@ -80,21 +94,21 @@ import axios from 'axios';
 
     }
 
-    #Contactanos >div >a >div{
+    #Contactanos >div >div:first-of-type >a >div{
         display: flex;
         align-self: center;
         justify-self: center;
         width: fit-content;
         height: fit-content;
     }
-    #Contactanos >div >a >div >div{
+    #Contactanos >div >div:first-of-type >a >div >div{
         display: grid;
         grid-template-columns: 25px 1fr;
         grid-template-rows: 1fr;
         grid-gap: 5px;
     }
 
-    #Contactanos >div >a >div >div >span:first-of-type{
+    #Contactanos >div >div:first-of-type >a >div >div >span:first-of-type{
         display: block;
         width: 25px;
         height: 25px;
@@ -107,21 +121,21 @@ import axios from 'axios';
         justify-self: center;
     }
 
-    #Contactanos >div >a >div >div >span:last-of-type{
+    #Contactanos >div >div:first-of-type >a >div >div >span:last-of-type{
         display: flex;
         align-self: center;
         justify-self: center;
         color: white;
     }
 
-    #Contactanos >div >ul:first-of-type{
+    #Contactanos >div >div:first-of-type >ul:first-of-type{
         display: grid;
         grid-template-columns: 1fr 1fr;
         grid-template-rows: 1fr;
         list-style: none;
         margin-top: 10px;
     }
-    #Contactanos >div >ul:first-of-type >li{
+    #Contactanos >div >div:first-of-type >ul:first-of-type >li{
         height: 40px;
         border: 1px solid transparent #333;
         text-align: center;
@@ -129,7 +143,7 @@ import axios from 'axios';
         
     }
 
-    #Contactanos >div >ul:first-of-type >li:first-of-type{
+    #Contactanos >div >div:first-of-type >ul:first-of-type >li:first-of-type{
         border-top-left-radius: 500px;
         border-bottom-left-radius: 500px;
         background: #f09433; 
@@ -139,13 +153,13 @@ import axios from 'axios';
         filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#f09433', endColorstr='#bc1888',GradientType=1 );
     }
 
-    #Contactanos >div >ul:first-of-type >li:last-of-type{
+    #Contactanos >div >div:first-of-type >ul:first-of-type >li:last-of-type{
         background: #0e76a8; 
         border-top-right-radius: 500px;
         border-bottom-right-radius: 500px;
     }
 
-    #Contactanos >div >ul:first-of-type >li >a{
+    #Contactanos >div >div:first-of-type >ul:first-of-type >li >a{
         width: 100%;
         display: grid;
         grid-template-columns: 1fr;
@@ -155,21 +169,21 @@ import axios from 'axios';
         font-weight: 700;
     }
 
-    #Contactanos >div >ul:first-of-type >li >a >div{
+    #Contactanos >div >div:first-of-type >ul:first-of-type >li >a >div{
         height: fit-content;
         width: fit-content;
         display: flex;
         align-self: center;
         justify-self: center;
     }
-    #Contactanos >div >ul:first-of-type >li >a >div >div{
+    #Contactanos >div >div:first-of-type >ul:first-of-type >li >a >div >div{
         display: grid;
         grid-template-columns: 20px 1fr;
         grid-template-rows: 1fr;
         grid-gap: 5px;
     }
 
-    #Contactanos >div >ul:first-of-type >li >a >div >div >span:first-of-type{
+    #Contactanos >div >div:first-of-type >ul:first-of-type >li >a >div >div >span:first-of-type{
         background-color: transparent;
         width: 20px;
         height: 20px;
@@ -178,31 +192,31 @@ import axios from 'axios';
         background-position: center;
     }
 
-    #Contactanos >div >ul:first-of-type >li >a >div >div >span:last-of-type{
+    #Contactanos >div >div:first-of-type >ul:first-of-type >li >a >div >div >span:last-of-type{
         display: flex;
         align-self: center;
         justify-self: center;
     }
 
-    #Contactanos >div >ul:first-of-type >li:first-of-type >a >div >div >span:first-of-type{
+    #Contactanos >div >div:first-of-type >ul:first-of-type >li:first-of-type >a >div >div >span:first-of-type{
         background-image: url('../assets/img/instagram_logo.png');
     }
 
-    #Contactanos >div >ul:first-of-type >li:last-of-type >a >div >div >span:first-of-type{
+    #Contactanos >div >div:first-of-type >ul:first-of-type >li:last-of-type >a >div >div >span:first-of-type{
         background-image: url('../assets/img/linkedIn_logo.png');
     }
 
-    #Contactanos >div >ul:last-of-type{
+    #Contactanos >div >div:first-of-type >ul:last-of-type{
         display: grid;
         grid-template-columns: 1fr;
         grid-template-rows: 1fr 1fr;
         list-style: none;
-        margin-top: 10px;
+        margin-top: 20px;
         grid-gap: 5px;
         padding: 0 5px 0 5px;
     }
 
-    #Contactanos >div >ul:last-of-type >li{
+    #Contactanos >div >div:first-of-type >ul:last-of-type >li{
         display: flex;
         display: grid;
         grid-template-columns: 20px 1fr;
@@ -210,7 +224,7 @@ import axios from 'axios';
         grid-gap: 5px;
     }
 
-    #Contactanos >div >ul:last-of-type >li >span:first-of-type{
+    #Contactanos >div >div:first-of-type >ul:last-of-type >li >span:first-of-type{
         font-size: 0;
         width: 20px;
         height: 20px;
@@ -219,16 +233,77 @@ import axios from 'axios';
         background-position: center;
     }
 
-    #Contactanos >div >ul:last-of-type >li >span:last-of-type{
+    #Contactanos >div >div:first-of-type >ul:last-of-type >li >span:last-of-type{
         display: flex;
         align-self: center;
         justify-self: flex-start;
         font-weight: 700;
     }
-    #Contactanos >div >ul:last-of-type >li:first-of-type >span:first-of-type{
+    #Contactanos >div >div:first-of-type >ul:last-of-type >li:first-of-type >span:first-of-type{
         background-image: url('../assets/img/telephone_icon.png');
     }
-    #Contactanos >div >ul:last-of-type >li:last-of-type >span:first-of-type{
+    #Contactanos >div >div:first-of-type >ul:last-of-type >li:last-of-type >span:first-of-type{
         background-image: url('../assets/img/email_icon.png');
+    }
+
+    #Contactanos >div >div:last-of-type{
+        margin-top: 20px;
+    }
+
+    #Contactanos >div >div:last-of-type >form{
+        display: grid;
+        grid-template-columns: 1fr;
+        grid-template-rows: 1fr 1fr auto 1fr;
+        grid-gap: 10px;
+        margin-top: 10px;
+    }
+
+    #Contactanos >div >div:last-of-type >form >div >input[type=text]{
+        width: 100%;
+        border: none;
+        border-bottom: 1px solid #333;
+        background-color: transparent;
+        padding: 5px;
+        outline: none;
+    }
+
+    #Contactanos >div >div:last-of-type >form >div >textarea{
+        width: 100%;
+        border: 1px solid #333;
+        border-radius: 5px;
+        background-color: transparent;
+        padding: 5px;
+        outline: none;
+        resize: none;
+    }
+
+    #Contactanos >div >div:last-of-type >form >input[type=submit]{
+        width: 100%;
+        height: 40px;
+        background-color: #0e76a8;
+        border-radius: 5000px;
+        color: white;
+    }
+
+    @media (min-width: 900px){
+        #Contactanos{
+            max-width: 900px;
+            margin-left: auto;
+            margin-right: auto;
+        }
+
+        #Contactanos >div{
+            grid-template-columns: 1fr 1fr;
+            grid-template-rows: 1fr;
+            grid-gap: 20px;
+        } 
+
+        #Contactanos >div >div:first-of-type{
+            height: fit-content;
+        }
+
+        #Contactanos >div >div:last-of-type{
+            margin-top: 0;
+        }
     }
 </style>
